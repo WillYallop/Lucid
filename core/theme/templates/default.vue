399c38daed014da2f9b99c00c480b2eb8902e3ac
William Yallop <hello@williamyallop.com>
2021-10-19 00:01:49 +0100

    [theme]: updated plan for theme directory and started building some place holder components and templates.

---
+++ core/theme/templates/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+        <!--  -->
+        <header></header>
+
+        <!-- CONTENT -->
+        <? PAGE_CONTENT ?>
+        
+        <!--  -->
+        <footer></footer>
+
+    </div>
+</template>
