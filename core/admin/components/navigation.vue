0ac249d1f9dbccd44126a3db0c41a73f01cd6ba4
William Yallop <hello@williamyallop.com>
2021-10-23 15:33:01 +0100

    [admin]: started making a basic layout for the admin cms site.

---
+++ core/admin/components/navigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navCon">
+        nav
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.navCon {
+    width: 300px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    background-color: red;
+}
+</style>
--- core/admin/layouts/default.vue
+++ core/admin/layouts/default.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <Nuxt/>
+        <Header/>
+        <div class="siteContent">
+          <Navigation/>
+          <main class="mainContent">
+            <Nuxt/>
+          </main>
+        </div>
     </div>
 </template>
 
@@ -8,5 +14,14 @@
 body {
   padding: 0;
   margin: 0;
+}
+
+.siteContent {
+  display: flex;
+  .mainContent {
+    width: calc(100% - 300px);
+    margin: 0 0 0 300px;
+    padding: 30px;
+  }
 }
 </style>
