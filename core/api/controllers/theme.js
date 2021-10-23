c4779643c040bed68b633a191c2bb8f57a69da25
William Yallop <hello@williamyallop.com>
2021-10-23 22:50:49 +0100

    [admin]: added route to API to return all valid and non valid post types.

--- core/api/controllers/theme.js
+++ core/api/controllers/theme.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+
 const themeDirectory = path.resolve(__dirname, '../../theme');
+const configPostTypes = require(`${themeDirectory}/config/post-types.json`);
+
+// Modules
+const verifyFileExists = require('../../generator/util/verify-file');
+
+
 
 // ------------------------------------ ------------------------------------
 // GET list of theme page templates
@@ -26,5 +33,42 @@ exports.get_theme_layouts = (req, res, next) => {
             fileNames.push(file);
         });
         res.send(fileNames);
+    });
+}
+
+// ------------------------------------ ------------------------------------
+// GET list of valid post types
+// ------------------------------------ ------------------------------------
+exports.get_theme_posts = (req, res, next) => {
+    // Verify post name isnt a duplicate - if it is we cannot use it.
+    // Verify the template_name given exists
+    var validPosts = [];
+    var nonValidPosts = [];
+
+    for(let i = 0; i < configPostTypes.length; i++) {
+        let post = configPostTypes[i];
+
+        // Check if validPosts already contains a matching post name
+        let foundMatch = validPosts.find(x => x.name === post.name);
+        if(foundMatch) {
+            nonValidPosts.push({
+                postObject: post,
+                error: `Duplicate post name of ${post.name} found in post-types.json file!`
+            });
+        }
+        else {
+            if(verifyFileExists(`${themeDirectory}/templates/${post.template_name}`)) validPosts.push(post);
+            else {
+                nonValidPosts.push({
+                    postObject: post,
+                    error: `Template name of ${post.template_name} cannot be found in the theme/layouts directory!`
+                });
+            }
+        }
+    }
+
+    res.status(200).json({
+        posts: validPosts,
+        failed: nonValidPosts
     });
 }
--- core/api/routes/theme.js
+++ core/api/routes/theme.js
@@ -8,5 +8,6 @@ const ThemeController = require('../controllers/theme');
 
 router.get('/templates', ThemeController.get_theme_templates);
 router.get('/layouts', ThemeController.get_theme_layouts);
+router.get('/posts', ThemeController.get_theme_posts);
 
 module.exports = router;
