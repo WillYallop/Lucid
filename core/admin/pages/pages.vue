af9c39dcd3b3f2bee7f73a7a09c43718fd69940e
William Yallop <hello@williamyallop.com>
2021-10-23 19:43:00 +0100

    [admin]: added mainSection component with slots for content and header action button.

--- core/admin/pages/pages.vue
+++ core/admin/pages/pages.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
-        <h1>PAGE</h1>
+
+        <GeneralMainSection class="_02-01"
+        :title="'Pages'"
+        :body="'Create and manage all of your page'">
+            <!-- Main -->
+            <template v-slot:headerButton>
+                <button class="iconMain large _02-01">
+                    <img class="iconImg" src="@/assets/icons/plus.svg" alt="Pages">
+                </button>
+            </template>
+            <!-- Main -->
+            <template v-slot:main>
+                
+            </template>
+        </GeneralMainSection>
+
+
         <button @click="toggleLoading">Toggle Loading</button>
     </div>
 </template>
@@ -8,7 +24,7 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-    
+
     methods: {
         toggleLoading() {
             this.$store.dispatch('colo_toggleLoadingState', {
