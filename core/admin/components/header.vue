8f773bd802e1707a18c6cec7237f63485846bc6d
William Yallop <hello@williamyallop.com>
2021-10-23 18:29:52 +0100

    [admin]: added nav and header components, not responsive yet.

--- core/admin/components/header.vue
+++ core/admin/components/header.vue
@@ -1,6 +1,10 @@
 <template>
-    <header>
-        header
+    <header class="headerCon">
+        
+        <nuxt-link to="/" class="siteLogo">
+            <img class="siteLogo" src="@/assets/images/logo.svg" alt="Site Logo">
+        </nuxt-link>
+
     </header>
 </template>
 
@@ -11,5 +15,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.headerCon {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: $header-height;
+    background-color: map-get($map: $colours, $key: _05-01);
+    z-index: 10;
+    border-bottom: $border;
+    padding: 0 $padding;
+    display: flex;
+    align-items: center;
+    .siteLogo {
+        height: 30px;
+    }
+}
 </style>
--- core/admin/components/navigation.vue
+++ core/admin/components/navigation.vue
@@ -1,6 +1,71 @@
 <template>
     <nav class="navCon">
-        nav
+        <div class="navInner">
+            
+            <ul>
+                <!-- Dashboard / general -->
+                <div class="navLinkSect">
+                    <li>
+                        <nuxt-link to="/">
+                            <div class="iconMain _01-01">
+                                <img class="iconImg" src="@/assets/icons/tachometer.svg" alt="Tacometer">
+                            </div>
+                            Dashboard
+                        </nuxt-link>
+                    </li>
+                </div>
+                <!-- Pages / Posts -->
+                <div class="navLinkSect">
+                    <li>
+                        <nuxt-link to="/pages">
+                            <div class="iconMain _02-01">
+                                <img class="iconImg" src="@/assets/icons/page.svg" alt="Pages">
+                            </div>
+                            Pages
+                        </nuxt-link>
+                    </li>
+                </div>
+                <!-- Media -->
+                <div class="navLinkSect">
+                    <li>
+                        <nuxt-link to="/media">
+                            <div class="iconMain _03-01">
+                                <img class="iconImg" src="@/assets/icons/assets.svg" alt="Assets">
+                            </div>
+                            Media
+                        </nuxt-link>
+                    </li>
+                </div>
+                <!-- Theme / Settings -->
+                <div class="navLinkSect">
+                    <li>
+                        <nuxt-link to="/components">
+                            <div class="iconMain _04-01">
+                                <img class="iconImg" src="@/assets/icons/style.svg" alt="Style">
+                            </div>
+                            Components
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/style">
+                            <div class="iconMain _04-01">
+                                <img class="iconImg" src="@/assets/icons/style.svg" alt="Style">
+                            </div>
+                            Style
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/settings">
+                            <div class="iconMain _04-01">
+                                <img class="iconImg" src="@/assets/icons/cog.svg" alt="Settings">
+                            </div>
+                            Settings
+                        </nuxt-link>
+                    </li>
+                </div>
+            </ul>
+
+        </div>
     </nav>
 </template>
 
@@ -14,9 +79,54 @@ export default {
 .navCon {
     width: 300px;
     position: fixed;
-    top: 0;
+    top: $header-height;
     left: 0;
     bottom: 0;
-    background-color: red;
+    background-color: map-get($map: $colours, $key: _05-01);
+    border-right: $border;
+    .navInner {
+        padding: $padding;
+        ul {
+            .navLinkSect {
+                border-bottom: $border;
+                padding: 5px 0;
+                a {
+                    @include animate;
+                    padding: 5px 0;
+                    display: flex;
+                    align-items: center;
+                    font-size: 16px;
+                    color: map-get($map: $text, $key: _01-01);
+                    opacity: 0.5;
+                    &.nuxt-link-exact-active {
+                        opacity: 1;
+                    }
+                    .iconMain {
+                        margin-right: 10px
+                    }
+                    &:hover {
+                        opacity: 0.8;
+                        &.nuxt-link-exact-active {
+                            opacity: 1;
+                        }
+                        .iconMain {
+                            &._01-01 {
+                                background: map-get($map: $colours, $key: _01-02);
+                            }
+                            &._02-01 {
+                                background: map-get($map: $colours, $key: _02-02);
+                            }
+                            &._03-01 {
+                                background: map-get($map: $colours, $key: _03-02);
+                            }
+                             &._04-01 {
+                                background: map-get($map: $colours, $key: _04-02);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 </style>
