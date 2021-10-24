9029126b032a654146566a1b91f320465f181c0e
William Yallop <hello@williamyallop.com>
2021-10-24 11:14:35 +0100

    [admin]: created new vue3 project with typescript as nuxt wasnt really a requirement for the CMS before and its typescript support was causing issues.

---
+++ core/cms/src/layouts/main.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="layoutCon">
+
+        <Header/>
+        <div class="siteContent">
+          <Navigation/>
+          <main class="mainContent">
+            <Nuxt/>
+          </main>
+        </div>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class MainLayout extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
