571b0107c60675ed4c27d2811b9196688724495a
William Yallop <hello@williamyallop.com>
2021-10-26 21:55:32 +0100

    [cms]: added route to create a new component for pages.

--- core/api/routes/cms.js
+++ core/api/routes/cms.js
@@ -14,6 +14,7 @@ router.post('/page', jsonParser, CmsPageController.add_new_page);
 router.get('/page', jsonParser, CmsPageController.get_single_page); // Get single/multiple pages
 router.get('/page/:limit/:skip', jsonParser, CmsPageController.get_multiple_pages); // Get single/multiple pages
 // Components
-router.get('/components/:pageId', jsonParser, CmsComponentController.get_page_components); // Get all components that belong to a page.
+router.get('/components/:page_id', jsonParser, CmsComponentController.get_page_components); // Get all components that belong to a page.
+router.post('/components', jsonParser, CmsComponentController.add_page_component); // Create a new componenet entry for specific page
 
 module.exports = router;
