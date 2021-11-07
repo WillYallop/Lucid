103ab728b9e025eb5b72801fa6c33584a5a95793
William Yallop <hello@williamyallop.com>
2021-11-07 18:26:03 +0000

    [api]: added a new component route in the themes controller. Can now return registered and unregistered components.

---
+++ core/api/controllers/theme/component.js
@@ -0,0 +1,52 @@
+
+// Modules
+const { 
+    getUnregisteredComponents,
+    getRegisteredComponents
+} = require('../../../generator/util/components');
+
+// ------------------------------------ ------------------------------------
+// GET - get a list of unregistered components
+// ------------------------------------ ------------------------------------
+exports.get_unregistered_components = async (req, res, next) => {
+    try {
+        let unregisteredComponents = await getUnregisteredComponents();
+        res.status(200).json({
+            meta: {
+                has_components: unregisteredComponents.has_components,
+                total_unregistered: unregisteredComponents.unregistered.length
+            },
+            data: unregisteredComponents.unregistered
+        })
+    }
+    catch(err) {
+        res.status(500).json({
+            error: err
+        })
+    }
+}
+
+// ------------------------------------ ------------------------------------
+// GET - get a list of unregistered and registered component names
+// ------------------------------------ ------------------------------------
+exports.get_component_registered_state = async (req, res, next) => {
+    try {
+        let registeredComponents = await getRegisteredComponents('names');
+        res.status(200).json({
+            meta: {
+                has_components: registeredComponents.has_components,
+                total_unregistered: registeredComponents.unregistered.length,
+                total_registered: registeredComponents.registered.length
+            },
+            data: {
+                unregistered: registeredComponents.unregistered,
+                registered: registeredComponents.registered
+            }
+        })
+    }
+    catch(err) {
+        res.status(500).json({
+            error: err
+        })
+    }
+}
