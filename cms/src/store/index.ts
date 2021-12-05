a50081e17d0966f22b9e2948160404349c36873b
William Yallop <hello@williamyallop.com>
2021-12-05 22:35:06 +0000

    [cms]: setup basic files for the vuex store

--- cms/src/store/index.ts
+++ cms/src/store/index.ts
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import PersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
+// Modules
+import component from './modules/component'
+import generate from './modules/generate'
+import page from './modules/page'
+import post from './modules/post'
+import template from './modules/template'
+
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    component,
+    generate,
+    page,
+    post,
+    template
+  },
+  plugins: [
+    PersistedState({
+      paths: [],
+    })
+  ]
 })
