4a44a15ddff5f4fb7b0738e71979fcfdf0cec5de
William Yallop <hello@williamyallop.com>
2022-01-09 13:30:10 +0000

    [graphql]: added new directory and data file for page_components

---
+++ src/graphql/auth/page_component/data.ts
@@ -0,0 +1,35 @@
+import db from '../../../db';
+import moment from 'moment';
+import { __updateSetQueryGen } from '../shared/functions';
+
+
+// Update component_content_type for a page_components row
+export const updatePageComponent = async () => {
+    try {
+
+    }
+    catch(err) {
+        throw err;
+    }
+}
+
+// Add page_components table row 
+// Also add all component_content_type tables needed that component
+export const addPageComponent = async () => {
+    try {
+
+    }
+    catch(err) {
+        throw err;
+    }
+}
+
+// Delete corresponding page_components table row
+export const deleteComponent = async () => {
+    try {
+
+    }
+    catch(err) {
+        throw err;
+    }
+}
