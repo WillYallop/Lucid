8105f1075c271bb4b9c15c7e4a138ac1702a499d
William Yallop <hello@williamyallop.com>
2022-01-13 20:27:33 +0000

    [graphql]: added mutation fields for menus and menu items and added in empty functions for each field.

---
+++ src/graphql/auth/menu/data.ts
@@ -0,0 +1,48 @@
+import db from '../../../db';
+import moment from 'moment';
+import { __updateSetQueryGen } from '../shared/functions';
+
+
+// ------------------------------------ ------------------------------------
+// Menu
+// ------------------------------------ ------------------------------------
+
+// Delete menu
+export const deleteMenu = async () => {
+
+} 
+
+// Create menu
+export const createMenu = async () => {
+
+}
+
+// Update menu
+export const updateMenu = async () => {
+
+}
+
+// Get menu
+export const getMenu = async () => {
+
+}
+
+
+// ------------------------------------ ------------------------------------
+// Menu Items
+// ------------------------------------ ------------------------------------
+
+// Delete menu item
+export const deleteMenuItem = async () => {
+
+}
+
+// Add menu item
+export const addMenuItem = async () => {
+
+}
+
+// Update menu item
+export const updateMenuItem = async () => {
+
+}
---
+++ src/graphql/auth/menu/mutation.ts
@@ -0,0 +1,94 @@
+import { GraphQLFieldConfig, GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLID, GraphQLBoolean } from 'graphql';
+// @ts-ignore: Unreachable code error
+import { MenusModel, MenuLinkModel } from './type';
+import { DeleteResType } from '../shared/type';
+import { deleteMenu, createMenu, updateMenu, deleteMenuItem, addMenuItem, updateMenuItem, getMenu} from './data';
+
+// Menu
+// Delete menu and all its items
+const deleteMenuField: GraphQLFieldConfig<any, any, any> = {
+    type: DeleteResType,
+    description: 'Delete menu',
+    args: {
+
+    },
+    resolve: (_, args) => {
+        return deleteMenu(args);
+    }
+}
+
+// Create new menu
+const createMenuField: GraphQLFieldConfig<any, any, any> = {
+    type: MenusModel,
+    description: 'Delete menu',
+    args: {
+
+    },
+    resolve: (_, args) => {
+        return createMenu(args);
+    }
+}
+
+// Update menu
+const updateMenuField: GraphQLFieldConfig<any, any, any> = {
+    type: MenusModel,
+    description: 'Delete menu',
+    args: {
+
+    },
+    resolve: (_, args) => {
+        return updateMenu(args);
+    }
+}
+
+
+// Menu Items
+// Delete menu item
+const deleteMenuItemField: GraphQLFieldConfig<any, any, any> = {
+    type: DeleteResType,
+    description: 'Delete menu item',
+    args: {
+
+    },
+    resolve: (_, args) => {
+        return deleteMenuItem(args);
+    }
+}
+
+// Add menu item
+const addMenuItemField: GraphQLFieldConfig<any, any, any> = {
+    type: MenuLinkModel,
+    description: 'Add menu item',
+    args: {
+
+    },
+    resolve: (_, args) => {
+        return addMenuItem(args);
+    }
+}
+
+// Update menu item
+const updateMenuItemField: GraphQLFieldConfig<any, any, any> = {
+    type: MenuLinkModel,
+    description: 'Update menu item',
+    args: {
+
+    },
+    resolve: (_, args) => {
+        return updateMenuItem(args);
+    }
+}
+
+
+export const MenuMutation = new GraphQLObjectType({
+    name: 'MenuMutation',
+    description: 'The menu base mutation',
+    fields: {
+        delete_menu: deleteMenuField,
+        delete_menu_item: deleteMenuItemField,
+        create_menu: createMenuField,
+        add_menu_item: addMenuItemField,
+        update_menu: updateMenuField,
+        update_menu_item: updateMenuItemField
+    }
+})
