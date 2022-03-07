354f374e21426233fb86c3de869bc84b290c7a07
William Yallop <hello@williamyallop.com>
2022-03-07 21:29:36 +0000

    [core]: added in cli-color lib to spice up the log on dev and start and show some useful links and info

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 /// <reference path="../types/index.d.ts" />
 
+const path = require('path');
+const config = require(path.resolve("./lucid.config.js"));
+
+import clc from 'cli-color';
+
 import app from "./app";
 import http from 'http';
 import ip from 'ip';
@@ -15,10 +20,29 @@ import * as contentTypeController from './controller/content_type_config';
 import * as templateController from './controller/template';
 
 const start = () => {
+
     const port = process.env.PORT || 80;
     const server = http.createServer(app);
     server.listen(port);
-    console.log(`${ip.address()}:${port}`);
+
+    console.log((clc.yellow('-------------------------------------------------------------------------------------')));
+    console.log();
+    console.log(clc.yellow(clc.bold("Welcome to Lucid!")));
+    console.log();
+    console.log(clc.yellow("First time? Read our set up guide @ https://lucidcms.com/getting-started"));
+    console.log(clc.yellow("Documentation? Dive in @ https://lucidcms.com/documentation"));
+    console.log();
+    process.stdout.write(clc.yellow(clc.columns([
+        [clc.bold("Domain"), clc.bold("IP")],
+        [`${ config.https ? 'https://' : 'http://' }${ config.domain }`, `${ip.address()}:${port}`],
+        [`${ config.https ? 'https://' : 'http://' }cms.${ config.domain }`, `${ip.address()}:${port}`],
+        [`${ config.https ? 'https://' : 'http://' }api.${ config.domain }`, `${ip.address()}:${port}`],
+        [`${ config.https ? 'https://' : 'http://' }assets.${ config.domain }`, `${ip.address()}:${port}`],
+      ]))
+    );
+    console.log();
+    console.log((clc.yellow('-------------------------------------------------------------------------------------')));
+      
 }
 
 
