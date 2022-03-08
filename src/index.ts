e223b3096b48b70f5218b53732b5b6e6a0b5898d
William Yallop <hello@williamyallop.com>
2022-03-08 21:21:50 +0000

    [controllers]: removed old dist controller that was no longer being used

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ import * as generator from './generator';
 import * as componentController from './controller/component';
 import * as postsController from './controller/posts';
 import * as themeController from './controller/theme';
-import * as distController from './controller/dist';
 import * as contentTypeController from './controller/content_type_config';
 import * as templateController from './controller/template';
 
@@ -39,7 +38,6 @@ export {
     componentController,
     postsController,
     themeController,
-    distController,
     contentTypeController,
     generator,
     templateController,
