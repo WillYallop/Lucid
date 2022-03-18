bc48636278dfe041f307c483ce27b3dce7fa7e08
William Yallop <hello@williamyallop.com>
2022-03-18 20:06:04 +0000

    [cms]: change colour variables and restyled the navigation menu

--- cms/src/components/Layout/Navigation/Navigation.tsx
+++ cms/src/components/Layout/Navigation/Navigation.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 // Components
 import CoreIcon from "../../Core/Icon";
 import NavigationPostLinks from "./PostsLinks";
 // Icons
 import { faTachometerAlt, faFile, faPhotoVideo, faThLarge, faPalette, faCog } from '@fortawesome/free-solid-svg-icons';
+// data
+import { generateSite } from '../../../data/generator';
 
 interface NavigationProps {
     showNav: boolean
@@ -12,54 +15,75 @@ interface NavigationProps {
 
 const Navigation: React.FC<NavigationProps> = ({ showNav, toggleNav }) => {
 
+    const [ siteBuilt, setSiteBuilt ] = useState(false);
+    const [ publishRes, setPublishRes ] = useState<mod_generateSite>({} as mod_generateSite);
+
+    const publishSite = () => {
+        generateSite({
+            __args: {},
+            build_time: true,
+            pages_built: true
+        },
+        (response) => {
+            const data: mod_generateSite = response.data.data.generator.site || {};
+            setPublishRes(data);
+            setSiteBuilt(true);
+            alert('built');
+        },
+        (err) => {
+            console.log(err);
+        })
+    }
+
     return (
         <div className="navSideBar">
             <div className={`navSideBarSlider ${ showNav ? 'active' : '' }`}>
-                <nav className="navCon">
-                    <ul className="navUl">
+                <div className="navInner">
+                    <nav className="navCon">
+                        <ul className="navUl">
 
-                        {/* Dashboard */}
-                        <li className="navItem">
-                            <NavLink to='/' className={(navData) => navData.isActive ? "active" : "" }>
-                                <CoreIcon icon={faTachometerAlt}/> dashboard
-                            </NavLink >
-                        </li>
-                        <span className="seperator"></span>
-                        {/* Page section */}
-                        <li className="navItem">
-                            <NavLink to='/pages' className={(navData) => navData.isActive ? "active" : "" }>
-                                <CoreIcon icon={faFile}/> pages
-                            </NavLink >
-                        </li>
-                        <NavigationPostLinks/>
-                        <span className="seperator"></span>
-                        {/* Media */}
-                        <li className="navItem">
-                            <NavLink to='/media' className={(navData) => navData.isActive ? "active" : "" }>
-                                <CoreIcon icon={faPhotoVideo}/> media
-                            </NavLink >
-                        </li>
-                        <span className="seperator"></span>
-                        {/* Alt Section */}
-                        <li className="navItem">
-                            <NavLink to='/components' className={(navData) => navData.isActive ? "active" : "" }>
-                                <CoreIcon icon={faThLarge}/> components
-                            </NavLink >
-                        </li>
-                        <li className="navItem">
-                            <NavLink to='/style' className={(navData) => navData.isActive ? "active" : "" }>
-                                <CoreIcon icon={faPalette}/> style
-                            </NavLink >
-                        </li>
-                        <li className="navItem">
-                            <NavLink to='/settings' className={(navData) => navData.isActive ? "active" : "" }>
-                                <CoreIcon icon={faCog}/> settings
-                            </NavLink >
-                        </li>
-                        <span className="seperator"></span>
-
-                    </ul>
-                </nav>
+                            {/* Dashboard */}
+                            <li className="navItem">
+                                <NavLink to='/' className={(navData) => navData.isActive ? "active" : "" }>
+                                    <CoreIcon icon={faTachometerAlt} style={'transparent'}/> dashboard
+                                </NavLink >
+                            </li>
+                            <span className="seperator"></span>
+                            {/* Page section */}
+                            <li className="navItem">
+                                <NavLink to='/pages' className={(navData) => navData.isActive ? "active" : "" }>
+                                    <CoreIcon icon={faFile} style={'transparent'}/> pages
+                                </NavLink >
+                            </li>
+                            <NavigationPostLinks/>
+                            <span className="seperator"></span>
+                            {/* Other */}
+                            <li className="navItem">
+                                <NavLink to='/media' className={(navData) => navData.isActive ? "active" : "" }>
+                                    <CoreIcon icon={faPhotoVideo} style={'transparent'}/> media
+                                </NavLink >
+                            </li>
+                            <li className="navItem">
+                                <NavLink to='/components' className={(navData) => navData.isActive ? "active" : "" }>
+                                    <CoreIcon icon={faThLarge} style={'transparent'}/> components
+                                </NavLink >
+                            </li>
+                            <li className="navItem">
+                                <NavLink to='/style' className={(navData) => navData.isActive ? "active" : "" }>
+                                    <CoreIcon icon={faPalette} style={'transparent'}/> style
+                                </NavLink>
+                            </li>
+                            <li className="navItem">
+                                <NavLink to='/settings' className={(navData) => navData.isActive ? "active" : "" }>
+                                    <CoreIcon icon={faCog} style={'transparent'}/> settings
+                                </NavLink >
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <button className="publishSiteBtn btnStyle1" onClick={publishSite}>
+                    publish site
+                </button>
             </div>
             <div className={`mobileOverlay ${ showNav ? 'active' : '' }`} onClick={toggleNav}></div>
         </div>
--- cms/src/views/Dashboard.tsx
+++ cms/src/views/Dashboard.tsx
@@ -1,49 +1,14 @@
 
-import { useState } from "react";
 // Components
 import DefaultPage from "../components/Layout/DefaultPage";
-// data
-import { generateSite } from '../data/generator';
-
 
 const Dashboard: React.FC = () => {
-    
-    const [ siteBuilt, setSiteBuilt ] = useState(false);
-    const [ publishRes, setPublishRes ] = useState<mod_generateSite>({} as mod_generateSite);
-
-    const publishSite = () => {
-        generateSite({
-            __args: {},
-            build_time: true,
-            pages_built: true
-        },
-        (response) => {
-            const data: mod_generateSite = response.data.data.generator.site || {};
-            setPublishRes(data);
-            setSiteBuilt(true);
-        },
-        (err) => {
-            console.log(err);
-        })
-    }
 
     return (
         <DefaultPage
         title="dashboard"
         body="welcome to Lucid!">
-
-            
-
-            <button className="btnStyle1" onClick={publishSite}>Publish Site</button>
-            { 
-                siteBuilt ?
-                    <>
-                        <p>Build duration: { publishRes.build_time }</p>
-                        <p>Pages built: { publishRes.pages_built }</p>
-                        <a href="http://lucid.local/" target="_blank">Vist website</a>
-                    </>
-                : null 
-            }
+            HELLO
         </DefaultPage>
     );
 }
